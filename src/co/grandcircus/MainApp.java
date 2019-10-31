6d5af0b69f3fff1abd77d3f954351ea34b17417f
jacklearnsjava <jack.p.hagen@gmail.com>
2019-10-31 17:12:47 -0400

    update code by jack

--- src/co/grandcircus/MainApp.java
+++ src/co/grandcircus/MainApp.java
@@ -4,7 +4,6 @@
 package co.grandcircus;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -13,57 +12,56 @@ import java.util.TreeMap;
 public class MainApp {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scnr = new Scanner(System.in);
-		
+
 		List<Club> clubsList = new ArrayList<>();
 		Map<String, Members> membersMap = new TreeMap<>();
 		clubsList.add(new Club("Planet Fitness", "1234 Woodward Ave"));
 		clubsList.add(new Club("Golds Gym", "5678 Main St"));
 		clubsList.add(new Club("The Jacked Ones", "532 Mount Branch"));
 		clubsList.add(new Club("Lunk-free Zone", "333 Fourth St"));
-		
+
 		System.out.println("Welcome to a new and fitter version of you!");
-//		Members person1 = new Single("12", "Tony");
-//		Members person2 = new Multi("21", "George", 0);
-//		membersMap.put(person1.getId(), person1);
-//		membersMap.put(person2.getId(), person2);
-		
+		Members person1 = new Single("12", "Tony");
+		clubsList.get(3).getMembers().add(person1);
+		Members person2 = new Multi("21", "George", 0);
+		membersMap.put(person1.getId(), person1);
+		membersMap.put(person2.getId(), person2);
+
 		String cont = "yes";
 
 		while (cont.toLowerCase().startsWith("y")) {
 			// your code should start here!
 			String userResponse = Validator.getString(scnr, "Are you a new or returning member?").toLowerCase();
 			if (userResponse.equals("n")) {
-				String membershipChoice = Validator.getString(scnr, "Are you interested in our single or multi-club membership? ").toLowerCase();
-				if (membershipChoice.startsWith("s")) {
-					String userName = Validator.getString(scnr, "What is your name? ");
-					Members m = new Single("13", userName);
-					membersMap.put(m.getId(), m);
-					int counter = 1;
-					for (Club c : clubsList) {
-						System.out.printf("%d. %s\n", counter++, c.getName());
-					}
-					int userChoice = Validator.getInt(scnr, "Select club would you like to join? (Choose a number)", 1, 4);
-					System.out.println("You selected: " + clubsList.get(userChoice-1));
-					clubsList.get(userChoice-1).getMembers().add(m);
-				} else if (membershipChoice.startsWith("m")) {
-					String userName = Validator.getString(scnr, "What is your name? ");
-					Members m = new Multi("13", userName, 0);
-					membersMap.put(m.getId(), m);
-					int counter = 1;
-					System.out.println("You can join the following clubs: ");
-					for (Club c : clubsList) {
-						System.out.printf("%d. %s\n", counter++, c.getName());
-					}
-					for (int i = 0; i < clubsList.size(); i++) {
-						clubsList.get(i).getMembers().add(m);
-						
-					}
-					
+
+				addNewMember(clubsList, membersMap, scnr);
+
+			} else if (userResponse.equals("r")) {
+				int counter = 1;
+				for (Club c : clubsList) {
+					System.out.printf("%d. %s\n", counter++, c.getName());
 				}
+
+				int clubChoice = Validator.getInt(scnr, "Enter the branch number: ");
+				String userId = Validator.getString(scnr, "Please enter your two digit ID: ");
+
+				if (membersMap.containsKey(userId)) {
+					try {
+						membersMap.get(userId).setCheckedIn(true);
+						System.out.println("Welcome " + membersMap.get(userId).getName());
+
+					} catch (Exception e) {
+						System.out.println("Something went terribly wrong. Our bad.");
+
+					}
+				} else {
+					System.out.println("You are not currently in our system. Please see the Welcome Desk.");
+
+				}
+
 			}
-			// your logic should stop here if it doesn't need to be included in the loop
 			System.out.println("Do you want to continue (yes/no)");
 			cont = scnr.nextLine();
 		}
@@ -71,9 +69,41 @@ public class MainApp {
 			System.out.println(m);
 		}
 
-		//This is our indication that the program has ended
+		// This is our indication that the program has ended
 		System.out.println("Goodbye!");
-		
+
 	}
 
+	public static void addNewMember(List<Club> clubsList, Map<String, Members> membersMap, Scanner scnr) {
+
+		String membershipChoice = Validator
+				.getString(scnr, "Are you interested in our single or multi-club membership? ").toLowerCase();
+		if (membershipChoice.startsWith("s")) {
+			String userName = Validator.getString(scnr, "What is your name? ");
+			Members m = new Single("13", userName);
+			membersMap.put(m.getId(), m);
+			int counter = 1;
+			for (Club c : clubsList) {
+				System.out.printf("%d. %s\n", counter++, c.getName());
+			}
+			int userChoice = Validator.getInt(scnr, "Select club would you like to join? (Choose a number)", 1, 4);
+			System.out.println("You selected: " + clubsList.get(userChoice - 1));
+			clubsList.get(userChoice - 1).getMembers().add(m);
+		} else if (membershipChoice.startsWith("m")) {
+			String userName = Validator.getString(scnr, "What is your name? ");
+			Members m = new Multi("13", userName, 0);
+			membersMap.put(m.getId(), m);
+			int counter = 1;
+			System.out.println("You can join the following clubs: ");
+			for (Club c : clubsList) {
+				System.out.printf("%d. %s\n", counter++, c.getName());
+			}
+			for (int i = 0; i < clubsList.size(); i++) {
+				clubsList.get(i).getMembers().add(m);
+
+			}
+
+		}
+
+	}
 }
